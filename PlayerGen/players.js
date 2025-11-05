439602e91000ed82acfa4d5716c61dea51d78248
JesssseL <146840057+JesssseL@users.noreply.github.com>
2025-11-05 13:01:29 +0000

    add some basic player handeling

--- PlayerGen/players.js
+++ PlayerGen/players.js
@@ -22,3 +22,42 @@ function updatePlayers(speed) {
 // TODO - death handeling
 
 // TODO - collisions with ledges (falling of a ledge ect ect)
+
+/**
+ * This will be used to remove a player upon a death event
+ * @param {guid} id 
+ */
+function killPlayer(id) {
+  const index = PLAYERS.findIndex(p => p.id === id);
+
+  // If found, remove and handle death logic
+  if (index !== -1) {
+    const player = PLAYERS[index];
+    PLAYERS.splice(index, 1);
+    player.die();
+  } else {
+    console.warn(`Player with ID ${id} not found.`);
+  }
+}
+
+
+/**
+ * This will add a player to the game, the ID should be sent from the gesture controller
+ * As it will be used for additional operations (eg jumps) through the program
+ * @param {guid} id 
+ */
+function addPlayer(id) {
+    if (PLAYERS.length > MAX_PLAYERS) {
+        return false
+    }
+
+    const player = new Player(0, 0, id);
+    PLAYERS.push(player);
+}
+
+/**
+ * This is for testing - there should be no test players in real runs
+ */
+function renderTestPlayer() {
+    addPlayer(1)
+}
